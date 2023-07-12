105f3228b4679f52857f07ca2d0359192c030233
SoulRvr <pawelc29@interia.pl>
2023-07-12 12:41:10 +0200

    validatin update

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import illustration from "./images/illustration-dashboard.png";
 import { useState } from "react";
 
 function App() {
-  const err = document.querySelector(".error");
-  const input = document.querySelector("input[type=text]");
-  const [email, setEmail] = useState(" ");
+  const [email, setEmail] = useState("");
+  let [errMessage, setErrMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
-    if (/^[\w]+@[\w]+[.]{1}[a-zA-Z]{2,}$/.test(email)) {
+    const err = document.querySelector(".error");
+    const input = document.querySelector("input[type=text]");
+    const success = document.querySelector(".success");
+    // console.log(err);
+    // console.log(errMessage);
+    if (email == "") {
+      setErrMessage("Whoops! It looks like you forgot to add your email");
+      err.style.visibility = "visible";
+      input.style.borderColor = "var(--Light-Red)";
+    } else if (/^[\w]+@[\w]+[.]{1}[a-zA-Z]{2,}$/.test(email)) {
       err.style.visibility = "hidden";
       input.style.borderColor = "var(--Pale-Blue)";
+      success.style.zIndex = "1";
+      success.style.opacity = "100%";
+      setTimeout(() => {
+        success.style.zIndex = "-1";
+        success.style.opacity = "0%";
+      }, 2000);
     } else {
+      setErrMessage("Please provide a valid email address");
       err.style.visibility = "visible";
       input.style.borderColor = "var(--Light-Red)";
     }
@@ -27,9 +41,10 @@ function App() {
           onChange={(e) => setEmail(e.target.value)}
         ></input>
         <input type="submit" value="Notify Me" />
-        <div className="error">Please provide a valid email address</div>
+        <div className="error">{errMessage}</div>
       </form>
       <img src={illustration} alt="illustration" />
+      <div className="success">Subscribed</div>
     </main>
   );
 }
